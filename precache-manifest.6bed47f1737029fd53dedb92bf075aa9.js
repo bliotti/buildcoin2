ec4c3cd470bb241f195fb7e991d560dc527266d3
Brian Liotti <Brian Liotti>
2019-02-21 16:34:08 -0500

    changed

---
+++ precache-manifest.6bed47f1737029fd53dedb92bf075aa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a26bef541095d45ce42c",
+    "url": "/buildcoin-ideascale/static/css/main.3e733bf0.chunk.css"
+  },
+  {
+    "revision": "a26bef541095d45ce42c",
+    "url": "/buildcoin-ideascale/static/js/main.a26bef54.chunk.js"
+  },
+  {
+    "revision": "c2f89466c9bf0cc297e7",
+    "url": "/buildcoin-ideascale/static/js/1.c2f89466.chunk.js"
+  },
+  {
+    "revision": "71909333b59f5ac21dc1",
+    "url": "/buildcoin-ideascale/static/js/runtime~main.71909333.js"
+  },
+  {
+    "revision": "6bff29ac8b88a17f93f40ce706ac20ba",
+    "url": "/buildcoin-ideascale/static/media/react-logo.6bff29ac.jpg"
+  },
+  {
+    "revision": "88fbaef759790bb5055bf8c10ab5fa5f",
+    "url": "/buildcoin-ideascale/index.html"
+  }
+];
